40e3750f6d985cc9c80fe3a3abf3768f38102810
David Salinas <saotand@gmail.com>
2020-02-21 14:27:11 -0400

    upload docs folder

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "tu_repuesto/precache-manifest.aee35531477bc366519265b2705c4391.js"
+  "https://webminapps.github.io/tu_repuesto/precache-manifest.5c46471bbe884d913d213d0a72c2c7af.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tu_repuesto"});
